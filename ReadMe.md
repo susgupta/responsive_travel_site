0c09b7321d12a5e271dc432cd30bd57bb892a0cb
susgupta <guptask.7@gmail.com>
2022-02-21 14:46:03 -0500

    Adding media queries and scroll reveal

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,10 @@
 # Sushil G Responsive Travel Site (Design Exercise)
 
-Still Work in Progress......
+This is a responsive design exercise to create a fake travel/beach themed site.  Using media queries to cover break point design.  Same with site navigation for media queries. 
+
+Icons are using cool icons from [Remix Icons](https://remixicon.com).
+
+Also added cool scroll effects on wrapper elements using [Scroll Reveal](https://scrollrevealjs.org/).
 
 
 ## Technologies Used
@@ -8,6 +12,8 @@ Still Work in Progress......
 - HTML
 - CSS 
 - JavaScript
+- JS Library - Scroll Reveal -> https://scrollrevealjs.org/
+- Remix Icons -> https://remixicon.com
 
 ## Real-Time Viewing For Web Version
 
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const menuToggleIcon = document.getElementById('menu-toggle-icon');
 const navMobile = document.getElementById('nav');
 const headerElement = document.getElementById('header');
+const navLinks = document.querySelectorAll('.nav-mobile .list-link');
 
 
 // Toggle navigation
@@ -26,6 +27,43 @@ const headerScrollEvent = () => {
 window.addEventListener('scroll', headerScrollEvent);
 
 // Add selected link styles to the link clicked and close navigation when clicking the links
+navLinks.forEach(link => link.addEventListener('click', () => {
+    navMobile.classList.remove('active');
+    headerElement.classList.remove('active');
 
+    let current = document.getElementsByClassName('current');
+    current[0].className = current[0].className.replace(' current', '');
+    link.className += ' current';
+}));
 
 // Scroll Reveal
+const sr = ScrollReveal({
+    distance: '25px',
+    duration: 2500
+});
+
+sr.reveal(`.image-left, .popular-destinations-data, .plan-trip-data`, {
+    origin: 'left'
+});
+
+sr.reveal('.image-center', {
+    origin: 'bottom'
+});
+
+sr.reveal(`.image-right, .showcase-data`, {
+    origin: 'right'
+});
+
+sr.reveal(`.beach-data-wrapper`, {
+    origin: 'bottom',
+    interval: 250
+});
+
+sr.reveal(`.newsletter-container`, {
+    origin: 'top'
+});
+
+sr.reveal(`.footer-container-inner > div, .footer-separator, .copyright`, {
+    origin: 'bottom',
+    interval: 250
+});
